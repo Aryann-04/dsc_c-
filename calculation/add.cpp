d57083e7cdc6fb12c5a1cd61166064fe96e288fe
Aryann-04 <aryanzalavadia4@gmail.com>
2024-11-20 11:49:52 +0530

    az

---
+++ calculation/add.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a;
+   int b;
+
+   cout<< "value a:";
+   cin>>a;
+
+   cout<< "value b:";
+   cin>>b;
+
+   cout << a+b << "\n";
+   cout << a-b << "\n";
+   cout << a/b << "\n";
+   cout << a*b << "\n";
+   cout << a%b << "\n";
+
+   return 0;
+}
