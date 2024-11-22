17bc1f113a0a52f14f9261cde8b83592e8e6f78d
Aryann-04 <aryanzalavadia4@gmail.com>
2024-11-22 11:25:33 +0530

    que 1 to 4

---
+++ if else 2/2.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+//*1*/
+
+    int a;
+    cout << "Please enter an int: ";
+    cin >> a;
+
+    if (a % 2 == 0)
+    {
+        cout << "The number " << a << " is even." << "\n";
+    }
+    else
+    {
+        cout << "The number " << a << " is odd." << "\n";
+    }
+
+
+
+//*2*/
+
+
+    int score;
+
+    cout << "Enter your score (0 to 100): ";
+    cin >> score;
+
+    if (score < 0 || score > 100)
+    {
+        cout << "Invalid score. Please enter a score between 0 and 100." << endl;
+    }
+    else
+    {
+
+        if (score >= 90)
+        {
+            cout << "Your grade is: A" << endl;
+        }
+        else if (score >= 80)
+        {
+            cout << "Your grade is: B" << endl;
+        }
+        else if (score >= 70)
+        {
+            cout << "Your grade is: C" << endl;
+        }
+        else if (score >= 60)
+        {
+            cout << "Your grade is: D" << endl;
+        }
+        else
+        {
+            cout << "Your grade is: F" << endl;
+        }
+    }
+
+
+//*3*/
+
+
+int age;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age >= 18) {
+        cout << "You are eligible to vote." << endl;
+    } else {
+        cout << "You are not eligible to vote." << endl;
+    }
+
+
+//*4*/
+
+int year;
+
+    cout << "Enter a year: ";
+    cin >> year;
+
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+        cout << year << " is a leap year." << endl;
+    } else {
+        cout << year << " isn't a leap year." << endl;
+    }
+
+
+}
