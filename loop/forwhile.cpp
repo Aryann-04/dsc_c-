f405c0785914aba8709d933b544a45080a70cb01
Aryann-04 <aryanzalavadia4@gmail.com>
2024-11-28 10:37:36 +0530

    loop

--- loop/forwhile.cpp
+++ loop/forwhile.cpp
@@ -41,37 +41,37 @@ int main()
 
     //---------------------------------------------------------------QUE 4--------------------------------------------------------------------------
 
-//  int n;
-//     cout << "Enter any number: ";
-//     cin >> n;
+ int n;
+    cout << "Enter any number: ";
+    cin >> n;
 
-//     int a = n;
+    int a = n;
 
-//     while (a >= 1)
-//     {
-//         cout << a << " ";
-//         a--;
-//     }
+    while (a >= 1)
+    {
+        cout << a << " ";
+        a-=2;
+    }
 
-//     cout << endl;
+    cout << endl;
 
 
 
     //---------------------------------------------------------------QUE 5--------------------------------------------------------------------------
 
 
-    int start, end;
+//     int start, end;
 
-    cout << "Enter the first Year: ";
-    cin >> start;
-    cout << "Enter the Endyear: ";
-    cin >> end;
+//     cout << "Enter the first Year: ";
+//     cin >> start;
+//     cout << "Enter the Endyear: ";
+//     cin >> end;
 
-    cout << "Output:" << endl;
+//     cout << "Output:" << endl;
  
-    while (start <= end) {
-        cout << start << endl;
-        start += 4; 
+//     while (start <= end) {
+//         cout << start << endl;
+//         start += 4; 
                
-    }
+//     }
 }
