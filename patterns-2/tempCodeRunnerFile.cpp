174d28ab0c4b01ab6b5a94ca0678e0065c591301
Aryann-04 <aryanzalavadia4@gmail.com>
2024-12-06 10:48:38 +0530

    p2(4dec)

---
+++ patterns-2/tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+int n=5; 
+
+    // for (int i=0; i<n; i++) 
+    // { 
+    //     for (int j=n; j>i; j--) 
+    //     { 
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
